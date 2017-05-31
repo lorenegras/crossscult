c453a7309f14f953d72d82c8841ac566f686797b
Lorene Gras <gras.lorene@gmail.com>
2017-05-31 19:51:10 +0200

    initialisation du git

---
+++ Crosscult/plugins/cordova-plugin-indooratlas/README.md
@@ -0,0 +1,6 @@
+# IndoorAtlas Cordova Plugin 1.1 with IndoorAtlas SDK 2.3
+
+This Cordova plugin provides information about the device's location inside finger printed buildings and uses the [IndoorAtlas location APIs](http://docs.indooratlas.com/cordova/api-documentation).
+
+## Documentation
+Documentation for the plugin is available in [http://docs.indooratlas.com/cordova](http://docs.indooratlas.com/cordova).
---
+++ Crosscult/www/js/APIKeys.js
@@ -0,0 +1,12 @@
+
+// API KEYS FOR INDOORATLAS SERVICES AND GOOGLE MAPS JAVASCRIPT API
+
+// Get Google Maps API Key from here: https://console.developers.google.com/apis/credentials
+var GOOGLE_API_KEY = 'AIzaSyAtaLcv7_cTwTzPpsBWK__J4zWHyyxpPxM';
+
+// Get IndoorAtlas API Key and Secret from here https://app.indooratlas.com/apps
+var IA_API_KEY = 'c77ab63e-65ae-4418-a49a-fb7d1b2321dd';
+var IA_API_SECRET = 'NWZ+0rLDylVULClrEJIMNnRRIpUItU7DIOkcxUE4uSbf+zyjc/YGuOM3WR8iRfIkhHAuGzNIuDyJMYFbY17Uk7djxiYPYfOSb+X5aSSfK2GdR+CwVs3xUHZllpaCJA==';
+
+// Floorplan ID https://developer.indooratlas.com/venues
+var IA_FLOORPLAN_ID = '32b09492-0ca0-4462-95a4-0d88121ebf38';
